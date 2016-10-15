6ee1cf15ac4c7639ce18652e678a7946901e2c94
aaugust4 <arjunaugustine@gmail.com>
2016-10-15 12:22:31 -0400

    Generic Problem

--- code/5/problem.py
+++ code/5/problem.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
-import random
+import random, sys
 import copy, math, sys
 
 def shuffle(lst):
@@ -94,18 +94,78 @@ class Problem(O):
   """
   Class representing the Osyczka2 problem
   """
-  def __init__(self):
-    O.__init__(self)
-    self.decisions = []
-    self.decisions.append(Decision('x1', 0, 10))
-    self.decisions.append(Decision('x2', 0, 10))
-    self.decisions.append(Decision('x3', 1, 5))
-    self.decisions.append(Decision('x4', 0, 6))
-    self.decisions.append(Decision('x5', 1, 5))
-    self.decisions.append(Decision('x6', 0, 10))
-    self.objectives = [Objective('f1', True), Objective('f2', True)]
-    self.points = []
 
+  def __init__(self, decisions, objectives):
+    """
+    Initialize Problem.
+    :param decisions -  Metadata for Decisions
+    :param objectives - Metadata for Objectives
+    """
+    O.__init__(self)
+    self.decisions = decisions
+    self.objectives = objectives
+
+  @staticmethod
+  def evaluate(point):
+    assert False # Problem should not be instantiated separately. The parent evaluate method would override this.
+    return point.objectives
+
+  @staticmethod
+  def is_valid(self, point):
+    """
+    Checks whether the decisions lie in the low to high limits.
+    :param self:
+    :param point:
+    :return:
+    """
+    print " BLOODDDDYY HEELLLL FUUUCKCKKKKERRRR"
+    # print self
+    # print point
+    for i,d in enumerate(point.decisions):
+      if d < self.decisions[i].low or d > self.decisions[i].high:
+        return False
+    return True
+
+  def generate_one(self, retries=500):
+    """
+    Generate a valid random point, evaluate it and add it to points list.
+    :return: generated point
+    """
+    # print self
+    for _ in xrange(retries):
+      point = Point([random.randint(d.low, d.high) for d in self.decisions])
+      # print "", "", point
+      if self.is_valid(self, point):
+        self.evaluate(self, point)
+        self.points.append(point)
+        return point
+    print "Fuck this"
+    # sys.exit()
+    raise RuntimeError("Exceeded max runtimes of %d" % retries)
+
+
+class Osyczka2(Problem):
+
+  def __init__(self):
+    """
+    Initialize the POM3 classes
+    The model has 4 objectives
+    Cost in [0,10000] - Minimize
+    Score in [0,1] - Maximize
+    Completion in [0,1] - Maximize
+    Idle in [0,1] - Minimize
+    """
+    self.points = []
+    names = ['x1', 'x2', 'x3', 'x4', 'x5', 'x6']
+    lows = [0, 0, 1, 0, 1, 0]
+    highs = [10, 10, 5, 6, 5, 10]
+    # TODO 2: Use names, lows and highs defined above to code up decision
+    # and objective metadata for POM3.
+    decisions = [Decision(n, l, h) for n, l, h in zip(names, lows, highs)]
+    objectives = [Objective("f1", True), Objective("f2", True)]
+    Problem.__init__(self, decisions, objectives)
+
+  @staticmethod
   def evaluate(self, point):
     (x1, x2, x3, x4, x5, x6) = point.decisions
     f1 = -1*(25*((x1-2)**2) + (x2-2)**2 + (x3-1)**2 * (x4-4)**2 + (x5-1)**2)
@@ -115,7 +175,7 @@ class Problem(O):
     return point.objectives
 
   @staticmethod
-  def is_valid(point):
+  def is_valid(self, point):
     """
     Check if point lies in valid range. All points valid in this case.
     :param self:
@@ -123,25 +183,30 @@ class Problem(O):
     :return:
     """
     [x1, x2, x3, x4, x5, x6] = point.decisions
-    if x1 + x2 -2 < 0: return False
-    if 6 - x1 - x2 < 0: return False
-    if 2 - x2 + x1 < 0: return False
-    if 2 - x1 + 3*x2 < 0: return False
-    if 4 - (x3 - 3)**2 - x4 < 0: return False
-    if (x5 - 3)**3 + x6 - 4 < 0: return False
+    if x1 + x2 -2 < 0:
+      # print '1'
+      return False
+    if 6 - x1 - x2 < 0:
+      # print '2'
+      return False
+    if 2 - x2 + x1 < 0:
+      # print '3'
+      return False
+    if 2 - x1 + 3*x2 < 0:
+      # print '4'
+      return False
+    if 4 - (x3 - 3)**2 - x4 < 0:
+      # print '5'
+      return False
+    if (x5 - 3)**3 + x6 - 4 < 0:
+      # print '6'
+      return False
+    for i, d in enumerate(point.decisions):
+      if d < self.decisions[i].low or d > self.decisions[i].high:
+        print i, d, self.decisions[i].low, self.decisions[i].high
+        return False
     return True
-
-  def generate_one(self):
-    """
-    Generate a valid random point, evaluate it and add it to points list.
-    :return: generated point
-    """
-    while True:
-      mypoint = Point([random.randint(d.low, d.high) for d in self.decisions])
-      if self.is_valid(mypoint):
-        self.evaluate(mypoint)
-        self.points.append(mypoint)
-        return mypoint
+    # return Problem.is_valid(Problem, point)
 
   def change_decision(self, point, c):
     """
@@ -154,8 +219,8 @@ class Problem(O):
     while True:
       mypoint.decisions = point.decisions
       mypoint.decisions[int(c.name[1])-1] = random.randint(c.low, c.high)
-      if self.is_valid(mypoint):
-        self.evaluate(mypoint)
+      if self.is_valid(self, mypoint):
+        self.evaluate(self, mypoint)
         self.points.append(mypoint)
         say('!')
       else:
@@ -174,7 +239,7 @@ class Problem(O):
     id = int(c.name[1])-1
     for val in xrange(int(math.ceil(c.low)), int(math.floor(c.high))):
       mypoint.decisions[id] = val
-      if self.is_valid(mypoint):
+      if self.is_valid(self, mypoint):
         if mypoint.energy > bestpoint.energy:
           bestpoint = mypoint
     print_char = ',' if bestpoint.energy is point.energy else '|'
@@ -197,43 +262,39 @@ class MaxWalkSat(O):
     FI
   RETURN failure, best solution found
   """
-  def __init__(self, problem):
+  def __init__(self, model):
     O.__init__(self)
-    self.Prob = problem()
+    self.Model = model()
     self.P = 0.5
-    self.MAX_TRIES = 50
-    self.MAX_CHANGES = 50
-    self.best = self.Prob.generate_one()
+    self.MAX_TRIES = 25
+    self.MAX_CHANGES = 25
+    self.best = self.Model.generate_one()
 
   def update_best(self, point):
     if point.energy > self.best.energy:
       say('+')
       self.best = point
 
-  def run(self, problem):
+  def run(self, model):
     for i in xrange(self.MAX_TRIES):
-      # print 'i =', i,
       say(format(self.best.energy, '4d'))
-      solution = problem.generate_one()  # Generate and evaluate a new point.
+      solution = model.generate_one()  # Generate and evaluate a new point.
       say(format(solution.energy, '4d'))
       say('')
       self.update_best(solution)
       for j in xrange(self.MAX_CHANGES):
-        # print 'j =', j,
-        c = random.choice(problem.decisions)
-        # print c
+        c = random.choice(model.decisions)
         if self.P < random.random():
-          solution = problem.change_decision(solution, c)
+          solution = model.change_decision(solution, c)
           self.update_best(solution)
         else:
-          solution = problem.maximize_decision_score(solution, c)
+          solution = model.maximize_decision_score(solution, c)
           self.update_best(solution)
-      # say()
       print ""
     return self.best
 
 
-P = Problem()
-MWS = MaxWalkSat(Problem)
+P = Osyczka2()
+MWS = MaxWalkSat(Osyczka2)
 print MWS.run(P)
-
+print ""
